a4eeea4c2e9a1118640d2db52e98acaa2068f0df
hauntarl <sameer.mungole@gmail.com>
2020-08-23 14:26:45 +0530

    stub project

---
+++ android/app/src/main/kotlin/com/example/sidebar_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sidebar_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sidebar ui',
+      home: Scaffold(),
+    );
+  }
+}
